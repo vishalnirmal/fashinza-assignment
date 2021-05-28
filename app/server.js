54bcb8bba2881bcf094f1e2cd3622df4cbb0a6bd
Vishal Nirmal <nirmalvishal24@gmail.com>
2021-05-28 13:41:58 +0530

    hosting build from server

--- app/server.js
+++ app/server.js
@@ -2,14 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 const productRoutes = require('./routes/products');
+const homeRoutes = require('./routes/home');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname,'..','build')));
 app.use('/products', productRoutes);
+app.use('/', homeRoutes);
 
 mongoose.connect(process.env.DB_URI, {
     useFindAndModify: false,
